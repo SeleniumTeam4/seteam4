af977510822be185b743616743f291ca4d6ff20e
trietnguyen267 <triet.nguyen267@gmail.com>
2015-06-29 10:04:34 +0700

    final

--- src/tests/article/TC001.java
+++ src/tests/article/TC001.java
@@ -1,8 +1,8 @@
 package article;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.server.handler.MaximizeWindow;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import dataTest.commonVariables;
@@ -40,9 +40,10 @@ public class TC001 {
 				
 	}
 	
-	/*@AfterMethod
+	@AfterMethod
 	public void afterMethod(){
+		driver = browser.getDriver();
 		driver.close();
 		driver.quit();
-	}*/
+	}
 }
